a8b548d6b29bb969189a10d4877ba68774624a27
tyobigoro_i <tyobigorox@gmail.com>
2020-05-17 17:58:20 +0900

    c

--- tTransform/ViewController.swift
+++ tTransform/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    var inTransformation: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    
+    func reverse() {
+        if inTransformation { return }
+        inTransformation = true
+        
+        let transform: CGAffineTransform = .init(scaleX: -1, y: -1)
+        UIView.animate(withDuration: 0.5, animations: {
+            self.view.transform = self.view.transform.isIdentity ? transform : .identity
+        }, completion: { _ in
+            self.inTransformation = false
+        })
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        reverse()
+        return true
+    }
 }
-
